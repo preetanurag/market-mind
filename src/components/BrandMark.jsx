3d3564f43fdbccfb620dbaded5b8a6b818ba4bd2
Preet Anurag <preetanurag@Preets-MacBook-Air.local>
2026-06-30 23:25:17 +0530

    Added new icon

--- src/components/BrandMark.jsx
+++ src/components/BrandMark.jsx
@@ -1,23 +1,37 @@
 export default function BrandMark() {
   return (
-    <svg className="brand-mark" viewBox="0 0 64 64" aria-hidden="true" role="img">
+    <svg className="brand-mark source-mm-mark" viewBox="180 360 664 330" aria-hidden="true" role="img">
       <path
-        className="brand-brain-left"
-        d="M30.6 13.5c-2.8-3-8.1-2.6-10.2 1.3-4-.3-7.6 2.7-7.9 6.8-3.7 1.1-6.4 4.6-6.4 8.8 0 3 1.4 5.7 3.5 7.4-.5 4.3 2.8 8.1 7 8.3 1.7 4.3 7.1 5.9 10.8 3.1 1.9 1.2 4.3-.1 4.3-2.4V17.1c0-1.4-.4-2.5-1.1-3.6Z"
+        className="source-mm-shape"
+        d="M220 610
+          L220 440
+          Q220 404 256 404
+          Q272 404 284 416
+          L366 498
+          Q382 514 398 498
+          L484 412
+          Q512 384 540 412
+          L626 498
+          Q642 514 658 498
+          L740 416
+          Q752 404 768 404
+          Q804 404 804 440
+          L804 610
+          Q804 646 768 646
+          Q732 646 732 610
+          L732 516
+          L682 566
+          Q642 606 602 566
+          L512 476
+          L422 566
+          Q382 606 342 566
+          L292 516
+          L292 610
+          Q292 646 256 646
+          Q220 646 220 610
+          Z"
       />
-      <path
-        className="brand-brain-right"
-        d="M33.4 13.5c2.8-3 8.1-2.6 10.2 1.3 4-.3 7.6 2.7 7.9 6.8 3.7 1.1 6.4 4.6 6.4 8.8 0 3-1.4 5.7-3.5 7.4.5 4.3-2.8 8.1-7 8.3-1.7 4.3-7.1 5.9-10.8 3.1-1.9 1.2-4.3-.1-4.3-2.4V17.1c0-1.4.4-2.5 1.1-3.6Z"
-      />
-      <path className="brand-brain-split" d="M32 16v31" />
-      <path
-        className="brand-brain-folds"
-        d="M26.7 18.4c-3.6-.9-6.6.9-7.6 4.1m4.8 1.5c-3.6.1-6.1 2.1-7.1 5.4m9.4 2.1c-3.2.2-5.3 2-6.2 4.8m6.5 5.2c-2.8.3-4.9-.8-6.4-3.1m17.2-20c3.6-.9 6.6.9 7.6 4.1M40.1 24c3.6.1 6.1 2.1 7.1 5.4m-9.4 2.1c3.2.2 5.3 2 6.2 4.8m-6.5 5.2c2.8.3 4.9-.8 6.4-3.1"
-      />
-      <path className="brand-candle-red" d="M17.2 55V42.5" />
-      <path className="brand-candle-red-body" d="M13.9 46.5h6.6v5.9h-6.6z" />
-      <path className="brand-candle-green" d="M46.8 55V42.5" />
-      <path className="brand-candle-green-body" d="M43.5 44.2h6.6v7h-6.6z" />
+      <rect className="source-mm-shape" x="476" y="532" width="72" height="126" rx="36" />
     </svg>
   )
 }
