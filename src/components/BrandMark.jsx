ce90a48fbd0d9b182b112b183c5c9b760eae6834
Preet Anurag <preetanurag@Preets-MacBook-Air.local>
2026-06-30 20:41:49 +0530

    Refresh Market and Mind branding

--- src/components/BrandMark.jsx
+++ src/components/BrandMark.jsx
@@ -1,23 +1,23 @@
 export default function BrandMark() {
   return (
-    <svg className="brand-mark" viewBox="0 0 48 48" aria-hidden="true" role="img">
+    <svg className="brand-mark" viewBox="0 0 64 64" aria-hidden="true" role="img">
       <path
-        d="M16 28.5c-4.1 0-7.4-3.2-7.4-7.2 0-3.3 2.2-6.1 5.3-7 .9-3.6 4.1-6.2 8-6.2 2.6 0 5 1.2 6.6 3.1 1-.5 2-.8 3.2-.8 4.2 0 7.6 3.5 7.6 7.8 0 3.8-2.7 7-6.3 7.7-.8 3.4-3.9 5.9-7.5 5.9-2.1 0-4.1-.9-5.5-2.3-1.1.7-2.5 1-4 1Z"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2.6"
+        className="brand-brain-left"
+        d="M30.6 13.5c-2.8-3-8.1-2.6-10.2 1.3-4-.3-7.6 2.7-7.9 6.8-3.7 1.1-6.4 4.6-6.4 8.8 0 3 1.4 5.7 3.5 7.4-.5 4.3 2.8 8.1 7 8.3 1.7 4.3 7.1 5.9 10.8 3.1 1.9 1.2 4.3-.1 4.3-2.4V17.1c0-1.4-.4-2.5-1.1-3.6Z"
       />
       <path
-        d="M17 17.2c2.4.5 4 2.4 4 4.8m7.5-8.2c-2.2.8-3.5 2.6-3.5 5.1m8.2 2.9c-2 .2-3.7 1.3-4.7 3.1M16.1 24.2c2.1-.2 3.8.5 5 2"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeWidth="2"
+        className="brand-brain-right"
+        d="M33.4 13.5c2.8-3 8.1-2.6 10.2 1.3 4-.3 7.6 2.7 7.9 6.8 3.7 1.1 6.4 4.6 6.4 8.8 0 3-1.4 5.7-3.5 7.4.5 4.3-2.8 8.1-7 8.3-1.7 4.3-7.1 5.9-10.8 3.1-1.9 1.2-4.3-.1-4.3-2.4V17.1c0-1.4.4-2.5 1.1-3.6Z"
       />
-      <path d="M14 39V30m10 9V26m10 13V23" stroke="currentColor" strokeLinecap="round" strokeWidth="2.6" />
-      <path d="M10.5 34.5h7m10-6h7m-14 5h7" stroke="currentColor" strokeLinecap="round" strokeWidth="2.4" />
+      <path className="brand-brain-split" d="M32 16v31" />
+      <path
+        className="brand-brain-folds"
+        d="M26.7 18.4c-3.6-.9-6.6.9-7.6 4.1m4.8 1.5c-3.6.1-6.1 2.1-7.1 5.4m9.4 2.1c-3.2.2-5.3 2-6.2 4.8m6.5 5.2c-2.8.3-4.9-.8-6.4-3.1m17.2-20c3.6-.9 6.6.9 7.6 4.1M40.1 24c3.6.1 6.1 2.1 7.1 5.4m-9.4 2.1c3.2.2 5.3 2 6.2 4.8m-6.5 5.2c2.8.3 4.9-.8 6.4-3.1"
+      />
+      <path className="brand-candle-red" d="M17.2 55V42.5" />
+      <path className="brand-candle-red-body" d="M13.9 46.5h6.6v5.9h-6.6z" />
+      <path className="brand-candle-green" d="M46.8 55V42.5" />
+      <path className="brand-candle-green-body" d="M43.5 44.2h6.6v7h-6.6z" />
     </svg>
   )
 }
